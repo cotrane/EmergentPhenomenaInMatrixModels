a463298bc5c2bfa64919568af6335e99f48fc5da
cotrane <tkaltenbrunner@gmail.com>
2014-09-17 18:23:27 +0100

    change lib path

--- ErrorEval/Release/ReadInputCommandLine.d
+++
@@ -1,4 +0,0 @@
-ReadInputCommandLine.d: ../ReadInputCommandLine.c \
- /home/tkaltenbrunner/CLAPACK-3.2.1/INCLUDE/f2c.h
-
-/home/tkaltenbrunner/CLAPACK-3.2.1/INCLUDE/f2c.h:
--- ErrorEval/Release/main.d
+++
@@ -1,6 +0,0 @@
-main.d: ../main.c /home/tkaltenbrunner/CLAPACK-3.2.1/INCLUDE/f2c.h \
- /home/tkaltenbrunner/CLAPACK-3.2.1/INCLUDE/clapack.h
-
-/home/tkaltenbrunner/CLAPACK-3.2.1/INCLUDE/f2c.h:
-
-/home/tkaltenbrunner/CLAPACK-3.2.1/INCLUDE/clapack.h:
